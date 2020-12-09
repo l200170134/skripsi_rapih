d1e5c85ff98a8e393e8c92d05f767ce8f535916e
l200170134 <hafidzalafaf@gmail.com>
2020-12-09 19:39:28 +0700

    update 3

--- application/views/halaman/v_kpivalue.php
+++ application/views/halaman/v_kpivalue.php
@@ -27,41 +27,30 @@ $role_id  = $this->session->userdata('role_id');
                 <div class="col-12">
                     <div class="card">
                         <div class="card-header">
-<<<<<<< HEAD
-                            <div class="col-12 col-lg-6">
-                                <?php if ($role_id == 2) { ?>
-                                    <a href="<?php echo base_url('Data_karyawan/detail_karyawan/' . $id_divisi); ?>" class="mr-2 bg-info p-1 rounded-circle"><i class="fas fa-arrow-left p-1" style="color:#fff;display:inline;" title="Kembali"></i></a>
-                                    <h5 style="display:inline;">Key Performance Index</h5>
-                                <?php } else { ?>
-                                    <h5 style="display:inline;">Key Performance Index</h5>
-                                <?php  } ?>
-                            </div>
-=======
                             <?php
-                                $role_id = $this->session->userdata('role_id');
-                                if ($role_id == 3) {
+                            $role_id = $this->session->userdata('role_id');
+                            if ($role_id == 3) {
                             ?>
                                 <div class="col-12 col-lg-6">
-                                    <a href="<?php echo base_url('Data_karyawan/detail_karyawan/'.$user['id_divisi']); ?>" class="mr-2 bg-info p-1 rounded-circle"><i class="fas fa-arrow-left p-1" style="color:#fff;display:inline;" title="Kembali"></i></a>
+                                    <a href="<?php echo base_url('Data_karyawan/detail_karyawan/' . $user['id_divisi']); ?>" class="mr-2 bg-info p-1 rounded-circle"><i class="fas fa-arrow-left p-1" style="color:#fff;display:inline;" title="Kembali"></i></a>
                                     <h5 style="display:inline;">Key Performance Index Karyawan</h5>
                                 </div>
                             <?php
-                                } elseif ($role_id == 2) {
+                            } elseif ($role_id == 2) {
                             ?>
                                 <div class="col-12 col-lg-6">
                                     <a href="<?php echo base_url('Evaluasi'); ?>" class="mr-2 bg-info p-1 rounded-circle"><i class="fas fa-arrow-left p-1" style="color:#fff;display:inline;" title="Kembali"></i></a>
                                     <h5 style="display:inline;">Key Performance Index Karyawan</h5>
                                 </div>
-                            <!-- <?php
-                                ?> -->
+                                <!-- <?php
+                                        ?> -->
                             <?php
-                                } else {
+                            } else {
                             ?>
                                 <h5 style="display:inline;">Key Performance Index Karyawan</h5>
-                            <?php 
+                            <?php
                             } ?>
-                            
->>>>>>> afa2887db94507aa50ada2687e060a19fd027846
+
                         </div>
                         <!-- /.card-header -->
                         <div class="card-body">
@@ -88,7 +77,7 @@ $role_id  = $this->session->userdata('role_id');
                             <div style="overflow: scroll;">
                                 <table class="table col-12 col-lg-6 table-hover" style="table-layout: fixed; word-wrap: break-word;">
                                     <thead>
-                                        
+
                                         <tr class="bg-secondary" align="center">
                                             <th width="50px">No</th>
                                             <th width="150px">Periode</th>
@@ -110,9 +99,9 @@ $role_id  = $this->session->userdata('role_id');
                                             <tr align="center">
                                                 <td><?php echo $no++; ?></td>
                                                 <td> <?php echo $bu['bulan'] . ' ' . $bu['tahun']; ?></td>
-                                                <td><?php echo round($bu['rata'],2);?>/5</td>
-                                                
-                                                
+                                                <td><?php echo round($bu['rata'], 2); ?>/5</td>
+
+
 
                                                 <!-- Tombol Aksi -->
                                                 <?php
@@ -151,53 +140,53 @@ $role_id  = $this->session->userdata('role_id');
 
 <!-- Button trigger modal -->
 
-<?php 
-     foreach ($value as $modal) : ?>
-<!-- Modal -->
-<div class="modal fade" id="exampleModal-<?= $modal['bulan'] ?>-<?= $modal['tahun'] ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Rincian</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <?= $modal['bulan'] ?> <?= $modal['tahun'] ?>
-        <table style="table-layout: fixed; word-wrap: break-word;" class="table table-hover">
-            <thead class="bg-secondary">
-                <tr>
-                    <?php 
-                    $id_divisi = $user['id_divisi'];
-                    $kpi = $this->db->get_where('tb_kpi', ['id_divisi' => $id_divisi])->result_array();
-                    foreach($kpi as $kp):
-                    ?>
-                    <th><?php echo $kp['pertanyaan']; ?></th>
-                    <?php endforeach; ?>                    
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <?php 
-                    $nip = $bu['nip'];
-                    $bulan = $bu['bulan'];
-                    $tahun = $bu['tahun'];
-                    $nilai = $this->db->get_where('tb_kpi_value', ['nip' => $nip, 'bulan' => $bulan, 'tahun' => $tahun])->result_array();
-                    foreach ($nilai as $nil):
-                    ?>
-                    <td><?php echo $nil['value']; ?></td>
-                    <?php endforeach; ?>
-                </tr>
-            </tbody>
-        </table>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-      </div>
+<?php
+foreach ($value as $modal) : ?>
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal-<?= $modal['bulan'] ?>-<?= $modal['tahun'] ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Rincian</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <?= $modal['bulan'] ?> <?= $modal['tahun'] ?>
+                    <table style="table-layout: fixed; word-wrap: break-word;" class="table table-hover">
+                        <thead class="bg-secondary">
+                            <tr>
+                                <?php
+                                $id_divisi = $user['id_divisi'];
+                                $kpi = $this->db->get_where('tb_kpi', ['id_divisi' => $id_divisi])->result_array();
+                                foreach ($kpi as $kp) :
+                                ?>
+                                    <th><?php echo $kp['pertanyaan']; ?></th>
+                                <?php endforeach; ?>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <?php
+                                $nip = $bu['nip'];
+                                $bulan = $bu['bulan'];
+                                $tahun = $bu['tahun'];
+                                $nilai = $this->db->get_where('tb_kpi_value', ['nip' => $nip, 'bulan' => $bulan, 'tahun' => $tahun])->result_array();
+                                foreach ($nilai as $nil) :
+                                ?>
+                                    <td><?php echo $nil['value']; ?></td>
+                                <?php endforeach; ?>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-</div>
-<?php 
-    endforeach;
- ?>
+<?php
+endforeach;
+?>
